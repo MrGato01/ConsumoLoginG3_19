2e9adc77f12b63993195983d4550900f689e5d42
MrGato01 <marconyenamorado@gmail.com>
2023-08-18 22:53:10 -0600

    Areglo y cambios registro

--- JS/Registro.js
+++ JS/Registro.js
@@ -1,18 +1,68 @@
 var UrlApiInsert = 'http://localhost:5003/usuario/post';
 
 
-function RegistrarUsuario(){
-    var datosusuario={
-    CodigoUsuario : $('#cousuario').val(),
-    Nombre : $('#nombre').val(),
-    Apellido : $('#apellido').val(),
-    Contrasena : $('#contrasena').val(),
-    Email : $('#email').val(),
-    Estado : $('#estado').prop('checked'),
-    ContrasenaExpira : $('#conexpira').prop('checked'),
-    DiasCaducidad : $('#diacaducidad').val(),
-    Rol : $('#rol').val(),
-    NumeroIntentoIncorrectos : $('#nintentos').val(),
+$(document).ready(function() {
+    var diasCaducidadInput = $('#diacaducidad');
+    var checkbox = $('#conexpira');
+    diasCaducidadInput.attr('disabled', 'disabled');
+
+    checkbox.change(function() {
+        if (checkbox.prop('checked')) {
+            diasCaducidadInput.attr('placeholder', 'Dias de caducidad (predeterminado 20)');
+            diasCaducidadInput.removeAttr('disabled');
+        } else {
+            diasCaducidadInput.attr('placeholder', 'No existe tiempo de caducidad');
+            diasCaducidadInput.val('');
+            diasCaducidadInput.attr('disabled', 'disabled');
+        }
+    });
+});
+
+
+
+function RegistrarUsuario() {
+    var codigoUsuario = $('#cousuario').val();
+    var nombre = $('#nombre').val();
+    var apellido = $('#apellido').val();
+    var contrasena = $('#contrasena').val();
+    var email = $('#email').val();
+    var rol = $('#rol').val() || 'Usuario';
+    var diasCaducidad = $('#conexpira').prop('checked') ? $('#diacaducidad').val() || '20' : '0';
+
+    if (codigoUsuario === '' || nombre === '' || apellido === '' || contrasena === '' || email === '') {
+        alert('Por favor, complete los primeros 5 campos.');
+        $(window).scrollTop(0)
+        return;
+    }
+
+    if (!validarCodigoUsuario(codigoUsuario)) {
+        alert('El codigo de usuario solo puede contener letras.');
+        $('#cousuario').val('');
+        $(window).scrollTop(0);
+        return;
+    }
+
+    if (codigoUsuario.length > 12) {
+        alert('El Codigo de usuario no puede tener más de 12 caracteres.');
+        $(window).scrollTop(0);
+        return;
+    }
+
+    if (!validarNombreYApellido()) {
+        return;
+    }
+
+
+    var datosusuario = {
+        CodigoUsuario: codigoUsuario,
+        Nombre: nombre,
+        Apellido: apellido,
+        Contrasena: contrasena,
+        Email: email,
+        Estado: $('#estado').prop('checked'),
+        ContrasenaExpira: $('#conexpira').prop('checked'),
+        DiasCaducidad: diasCaducidad,
+        Rol: rol,
     };
 
     var datosusuariojson = JSON.stringify(datosusuario);
@@ -34,4 +84,35 @@ function RegistrarUsuario(){
         }
     });
 
+}
+
+function validarCodigoUsuario(codigoUsuario) {
+    var letrasRegex = /^[A-Za-z]+$/;
+    return letrasRegex.test(codigoUsuario);
+}
+
+function validarNombreApellido(texto) {
+    var letrasRegex = /^[A-Za-z]+$/;
+    return letrasRegex.test(texto);
+}
+
+function validarNombreYApellido() {
+    var nombre = $('#nombre').val();
+    var apellido = $('#apellido').val();
+
+    if (!validarNombreApellido(nombre)) {
+        alert('El nombre solo puede tener letras.');
+        $('#nombre').val('');
+        $(window).scrollTop(0);
+        return false;
+    }
+
+    if (!validarNombreApellido(apellido)) {
+        alert('El apellido solo puede tener letras.');
+        $('#apellido').val('');
+        $(window).scrollTop(0);
+        return false;
+    }
+
+    return true;
 }
