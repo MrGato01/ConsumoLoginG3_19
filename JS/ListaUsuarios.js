685f2e19481a98e7b5ee24d639592eeab0602589
xokiara <kiara.moncada@unah.hn>
2023-08-21 19:22:08 -0600

    Correción de código y diseño

--- JS/ListaUsuarios.js
+++ JS/ListaUsuarios.js
@@ -1,10 +1,33 @@
 var UrlApiGetAll = 'http://localhost:5003/listaUsuarios/getAll';
 
 $(document).ready(function(){
-    CargarProductos();
+    CargarUsuarios();
 });
 
-function CargarProductos() {
+function convertirFecha(fecha) {
+    var dateObject = new Date(fecha);
+    var year = dateObject.getFullYear();
+    var month = String(dateObject.getMonth() + 1).padStart(2, '0');
+    var day = String(dateObject.getDate()).padStart(2, '0');
+    var hours = String(dateObject.getHours()).padStart(2, '0');
+    var minutes = String(dateObject.getMinutes()).padStart(2, '0');
+    var seconds = String(dateObject.getSeconds()).padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+
+$(document).ready(function() {
+
+    function convertirFecha(fecha) {
+        var dateObject = new Date(fecha);
+        var year = dateObject.getFullYear();
+        var month = String(dateObject.getMonth() + 1).padStart(2, '0');
+        var day = String(dateObject.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}-${hours}:${minutes}:${seconds}`;
+    }
+});
+
+function CargarUsuarios() {
     $.ajax({
         url:UrlApiGetAll,
         type:'GET',
@@ -21,14 +44,18 @@ function CargarProductos() {
                 '<td>'+ MiItems[i].contrasena +'</td>'+
                 '<td>'+ MiItems[i].email +'</td>'+
                 '<td>'+ MiItems[i].estado +'</td>'+
-                '<td>'+ MiItems[i].ultimafechaingreso +'</td>'+
+                '<td>'+ convertirFecha(MiItems[i].ultimafechaingreso) +'</td>'+
                 '<td>'+ MiItems[i].contrasenaexpira +'</td>'+
                 '<td>'+ MiItems[i].diascaducidad +'</td>'+
                 '<td>'+ MiItems[i].rol +'</td>'+
                 '<td>'+ MiItems[i].numerointentoincorrectos +'</td>'+
-                '<td>'+ MiItems[i].fecharegistro +'</td>'+
-                $('#datosusuario').html(Valores);
+                '<td>'+ convertirFecha(MiItems[i].fecharegistro) +'</td>'+
+                '</tr>';
             }
+            $('#datosusuario').html(Valores);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('Error:', textStatus, errorThrown);
         }
      });
 }
